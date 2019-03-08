bc0e884a9f69ad0c911d9d685b7f876c3d012a6d
MickMake <mick@mickmake.com>
2019-03-08 17:29:43 +1100

    Simplification.

--- 1.0.0/build/build-mailhog.sh
+++ 1.0.0/build/build-mailhog.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-echo "Hello from build-mailhog.sh"
-if [ -d "/build/rootfs/" ]
-then
-	rsync -HvaxP /build/rootfs/ /
-fi
+# See gearboxworks/gearbox-base for details.
+test -f /build/include-me.sh && . /build/include-me.sh
 
-apk --no-cache add ca-certificates
+c_ok "Started."
 
-apk --no-cache add --virtual build-dependencies go git
+c_ok "Installing packages."
+apk --no-cache add ca-certificates; checkExit
+apk --no-cache add --virtual build-dependencies go git; checkExit
 
-mkdir -p /build/gocode
+c_ok "Installing MailHog GoLang."
+mkdir -p /build/gocode; checkExit
 export GOPATH=/build/gocode
-go get github.com/mailhog/MailHog
-mv /build/gocode/bin/MailHog /usr/local/bin
-rm -rf /build/gocode
-apk del --purge build-dependencies
+go get github.com/mailhog/MailHog; checkExit
+mv /build/gocode/bin/MailHog /usr/local/bin; checkExit
 
-adduser -D -u 1001 mailhog
+c_ok "Adding mailhog user."
+adduser -D -u 1001 mailhog; checkExit
 
+c_ok "Cleaning up."
+rm -rf /build/gocode; checkExit
+apk del --purge build-dependencies; checkExit
+
+c_ok "Finished."
